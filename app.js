c42894bb5fd734a67fb331e360d8e3e63f42aa7b
Rushikesh Nagapurkar <148316559+Rushi1275@users.noreply.github.com>
2023-12-22 10:45:11 +0530

    Get the saved User Details from crudcrud.

--- app.js
+++ app.js
@@ -32,20 +32,43 @@ function getTodos() {
         });
 }
 
+window.addEventListener('DOMContentLoaded',()=>{
+    axios.get('https://crudcrud.com/api/15baff758d6f4ced990a18ee4e1f86a2/BookingDeta')
+    .then((response) => { 
+        const data = response.data;
+        displayData(data);
+    })
+    .catch((err) => {
+        console.error('Error fetching data:', err);
+    });
+})
+
 function displayData(data) {
     const dataList = document.getElementById('dataList');
     dataList.innerHTML = ''; // Clear previous data
 
     data.forEach((item) => {
         const listItem = document.createElement('li');
+        listItem.style.marginBottom="10px"
+
+        //delete btn 
         const delBtn=document.createElement('button');
         delBtn.textContent="Delete";
         delBtn.style.height="25px";
         delBtn.style.width="100px";
+        delBtn.style.marginLeft="10px"
+        delBtn.style.marginRight="10px"
+
+        //edit btn
+        const editBtn=document.createElement('button');
+        editBtn.textContent="Edit";
+        editBtn.style.height="25px";
+        editBtn.style.width="100px";
           
         listItem.textContent = `Name: ${item.name}, Email: ${item.email}, Number: ${item.number}`;
         dataList.appendChild(listItem);
-       listItem.appendChild(delBtn)
+       listItem.appendChild(delBtn);
+       listItem.appendChild(editBtn);
     });
 }
 
